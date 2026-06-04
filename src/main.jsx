b1bf4c49fe417deefb2827126000e107b6f6617b
Ashley Guth <ashguth82@gmail.com>
2026-06-04 12:53:13 +1000

    Import index.css in main.jsx
    Added missing import statement for index.css.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <-- This is the magic line you are missing!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
